bb093268270903723b74b15fd0f0210c2e3cc1ed
Three-Wheeled Sloth <36046890+SlothMD@users.noreply.github.com>
2026-07-25 11:11:00 -0400

    Load guided interview styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import "./guided-interview.css";
 import "./legal-notice.css";
 import "./performance.css";
 import "./privacy-review.css";
