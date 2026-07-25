d20f1fa67ab5b160a12ebd574e82040f353dcd83
Three-Wheeled Sloth <36046890+SlothMD@users.noreply.github.com>
2026-07-25 10:39:32 -0400

    Load performance instrumentation styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import "./performance.css";
 import "./privacy-review.css";
 import "./story-candidates.css";
 
